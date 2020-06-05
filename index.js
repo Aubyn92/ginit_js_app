835b4da71db1c76f2df74c982fe2755601bca3a3
Aubyn92 <livstaubyn92@gmail.com>
2020-06-05 11:41:37 +1000

    Created a utility package in lib/files.js and required it in the index.js file.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const chalk = require('chalk')
 const clear = require ('clear')
 const figlet = require('figlet')
+
+const files = require('./lib/files');
+
+clear();  //clear the screen
+
+console.log(
+    chalk.yellow(
+        figlet.textSync('Ginit', {horizontalLayout: 'full' })
+    )
+);
+
+
