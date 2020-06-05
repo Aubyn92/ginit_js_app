53610958792a671d88b5f1649766ce82fa335009
Aubyn92 <livstaubyn92@gmail.com>
2020-06-05 12:53:01 +1000

    Prompt for user input created. Installed configstore and created library for handling Github API

--- index.js
+++ index.js
@@ -12,4 +12,32 @@ console.log(
     )
 );
 
+if (files.directoryExists('.git')) {
+    console.log(chalk.red('Already a Git repository!'));
+    process.exit();
+}
+
+// const inquirer = require('./lib/inquirer');
+
+const run = async () => {
+    const credentials = await inquirer.askGithubCredentials();
+    console.log(credentials);
+};
+
+run();
+
+// const github = require('./lib/github');
+
+const Configstore = require('configstore');
+const conf = new Configstore('ginit');
+
+const run = async () => {
+    let token = github.getStoredGithubToken();
+    if(!token) {
+      token = await github.getPersonalAccesToken();
+    }
+    console.log(token);
+  };
+  
+
 
